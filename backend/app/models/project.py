874ea6d21ee310e7a70a53d9d84f6bda48a81d1c
brunohenrycoder <bh371341@gmail.com>
2026-04-12 18:34:12 -0300

    backend filtrando os projetos.

--- backend/app/models/project.py
+++ backend/app/models/project.py
@@ -1,7 +1,20 @@
-from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Table
 from sqlalchemy.orm import relationship
 from datetime import datetime, timezone
 from app.db.database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    email = Column(String, unique=True, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    created_at = Column(DateTime, default=lambda: datetime.now(timezone.utc))
+
+    projects = relationship("Project", back_populates="owner", cascade="all, delete-orphan")
+
 
 class Project(Base):
     __tablename__ = "projects"
@@ -12,8 +25,31 @@ class Project(Base):
     owner_id = Column(Integer, ForeignKey("users.id"), nullable=False)
     created_at = Column(DateTime, default=lambda: datetime.now(timezone.utc))
 
-    # Relacionamentos
-    owner = relationship("User")
+   
+    owner = relationship("User", back_populates="projects")
+   
     members = relationship("ProjectMember", back_populates="project", cascade="all, delete-orphan")
     files = relationship("FileNode", back_populates="project", cascade="all, delete-orphan")
     tasks = relationship("Task", back_populates="project", cascade="all, delete-orphan")
+
+
+class ProjectMember(Base):
+    __tablename__ = "project_members"
+    id = Column(Integer, primary_key=True, index=True)
+    project_id = Column(Integer, ForeignKey("projects.id"))
+    user_id = Column(Integer, ForeignKey("users.id"))
+    project = relationship("Project", back_populates="members")
+
+class FileNode(Base):
+    __tablename__ = "files"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    project_id = Column(Integer, ForeignKey("projects.id"))
+    project = relationship("Project", back_populates="files")
+
+class Task(Base):
+    __tablename__ = "tasks"
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    project_id = Column(Integer, ForeignKey("projects.id"))
+    project = relationship("Project", back_populates="tasks")
